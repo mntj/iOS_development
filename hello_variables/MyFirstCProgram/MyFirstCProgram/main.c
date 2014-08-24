26b9c4f0ff84165328d6b725f207b76c55f1fb3b
mntj <mntj11@gmail.com>
2014-08-24 12:18:39 -0400

    add anatomy_of_c and hello_variables directories

---
+++ hello_variables/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  MyFirstCProgram
+//
+//  Created by turner on 9/26/13.
+//  Copyright (c) 2013 treehouse. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int days_in_a_week = 7;
+    float cm_to_in = 2.54;
+    
+    char the_w;
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
