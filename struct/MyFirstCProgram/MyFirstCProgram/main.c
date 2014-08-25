23c7ecd8d5901bf0bea73067c38f0e8a25fa23b4
mntj <mntj11@gmail.com>
2014-08-25 13:48:38 -0400

    structs

---
+++ struct/MyFirstCProgram/MyFirstCProgram/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  MyFirstCProgram
+//
+//  Created by turner on 9/26/13.
+//  Copyright (c) 2013 treehouse. All rights reserved.
+//
+
+#include <stdio.h>
+typedef struct {
+    float center[3];
+    float radius;
+} Sphere;
+
+Sphere makeSphere(float *c, float r); // * notation used when passing an array to a function
+int main()
+{
+
+    float c[] = { 23, 45, 67};
+    float r = 32;
+    Sphere ball = makeSphere(c, r);
+    printf("center %f %f %f radius %f\n", ball.center[0], ball.center[1], ball.center[2], ball.radius);
+    
+    return 0;
+}
+
+Sphere makeSphere(float *c, float r) {
+    Sphere sphere;
+    sphere.center[0] = c[0];
+    sphere.center[1] = c[1];
+    sphere.center[2] = c[2];
+    
+    sphere.radius = r;
+    
+    return sphere;
+}
